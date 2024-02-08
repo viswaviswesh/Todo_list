b94f66e0e5edefbc1bccdbb85abbb9882f165e64
viswaviswesh <114049336+viswaviswesh@users.noreply.github.com>
2024-02-08 15:56:27 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,176 +1,158 @@
-import  Express  from "express";
+import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
-import _  from "lodash";
+import _ from "lodash";
 
-const app = Express();
-const port = 3000;
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.set("view engine" , "ejs");
+app.set("view engine", "ejs");
 
-app.use(Express.static("public"));
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect("mongodb://127.0.0.1:27017/todolistdb");
-
-const itemsSchema = new mongoose.Schema({
-    names: {
-        type: String,
-        required: true,
-    },
-
+mongoose.connect("mongodb://127.0.0.1:27017/todolistdb", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
 
+const itemsSchema = new mongoose.Schema({
+  names: {
+    type: String,
+    required: true
+  }
+});
 
 const Item = mongoose.model("Item", itemsSchema);
 
 const item = new Item({
-    names : "Wakeup at 7.00 Am " 
+  names: "Wakeup at 7.00 AM"
 });
 
 const item2 = new Item({
-    names : "Make up the Bed" 
+  names: "Make up the Bed"
 });
 
 const item3 = new Item({
-    names : "and do morning duties" 
+  names: "and do morning duties"
 });
 
-const defaultItems = [item,item2,item3];
+const defaultItems = [item, item2, item3];
 
 const listSchema = new mongoose.Schema({
-    names : String,
-    items : [itemsSchema]
+  names: String,
+  items: [itemsSchema]
 });
 
 const List = mongoose.model("List", listSchema);
 
-// Item.deleteMany({names: "Wakeup at 7.00 Am"});
-
-
-
 app.get("/", (req, res) => {
-    Item.find({})
-    .then(function (foundItems){
-        if(foundItems.length === 0){
-             Item.insertMany(defaultItems);
-        }else{
-            res.render("index.ejs",{ FormattedDate : formattedDate , newItems: foundItems })
-        }   
-    }) 
-    .catch(function(err){
-        console.log(err);
+  Item.find({})
+    .then(foundItems => {
+      if (foundItems.length === 0) {
+        return Item.insertMany(defaultItems);
+      }
+      return foundItems;
     })
-});
-
-
-app.get("/:id",(req,res) => {
-    const route = _.capitalize(req.params.id);
-
-    List.findOne({names : route})
-    .then(function(foundList){
-        if(!foundList){
-            const list = new List({
-                names : route,
-                items : defaultItems
-            });
-            list.save();
-            console.log("saved");
-            res.redirect("/" + route);
-        }else{
-            res.render("index.ejs",{FormattedDate : foundList.names , newItems: foundList.items})
-            console.log("already have")
-        }
+    .then(foundItems => {
+      res.render("index.ejs", { FormattedDate: formattedDate, newItems: foundItems });
     })
-    .catch(function (error){
-        console.log(err)
+    .catch(err => {
+      console.error(err);
+      res.status(500).send("Internal Server Error");
     });
-   
 });
-// app.get("/work", (req, res) => {
-//     res.render("work.ejs" ,{ workTasks})
-// });
+
+app.get("/:id", (req, res) => {
+  const route = _.capitalize(req.params.id);
+
+  List.findOne({ names: route })
+    .then(foundList => {
+      if (!foundList) {
+        const list = new List({
+          names: route,
+          items: defaultItems
+        });
+        return list.save();
+      }
+      return foundList;
+    })
+    .then(foundList => {
+      res.render("index.ejs", { FormattedDate: foundList.names, newItems: foundList.items });
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).send("Internal Server Error");
+    });
+});
 
 app.post("/submit", (req, res) => {
-    const itemName  = req.body.newItem;
-    const listName = req.body.list;
+  const itemName = req.body.newItem;
+  const listName = req.body.list;
 
-    const userItem = new Item({
-        names : itemName,
-    });
-
-    if(listName === formattedDate){
-        userItem.save();
-        res.redirect("/");
-    }else{
-        List.findOne({ names: listName })
-        .then((foundList) => {
-          foundList.items.push(userItem);
-          foundList.save();
-          res.redirect("/" + listName);
-        })
-        .catch((err) => {
-          console.error(err);
-        });
-    }
+  const userItem = new Item({
+    names: itemName,
   });
 
-// app.post("/submit2", (req, res) => {
-//     const { newItem } = req.body;
-//     if (newItem) {
-//         workTasks.push(newItem);
-//     }
-//     res.redirect('/work');
-// });
+  if (listName === formattedDate) {
+    userItem.save();
+    res.redirect("/");
+  } else {
+    List.findOne({ names: listName })
+      .then(foundList => {
+        foundList.items.push(userItem);
+        return foundList.save();
+      })
+      .then(() => {
+        res.redirect("/" + listName);
+      })
+      .catch(err => {
+        console.error(err);
+        res.status(500).send("Internal Server Error");
+      });
+  }
+});
 
-app.post("/delete",async (req,res) => {
-    const checkboxId = (req.body.newItem);
-    const listName = req.body.listName;
-    if(listName === formattedDate){
-        try{
-            await Item.findByIdAndDelete(checkboxId);
-            res.redirect("/");
-        } catch (error) {
-            console.log(error);
-        }   
-    }else{
-        List.findOne({names: listName})
-        .then(foundList =>{
-            foundList.items.pull({_id: checkboxId});
-            foundList.save();
-            res.redirect("/" + listName);
-        })
-        .catch(err => {
-            console.log(err);
-        });
+app.post("/delete", async (req, res) => {
+  const checkboxId = req.body.newItem;
+  const listName = req.body.listName;
+
+  try {
+    if (listName === formattedDate) {
+      await Item.findByIdAndDelete(checkboxId);
+    } else {
+      const foundList = await List.findOne({ names: listName });
+      foundList.items.pull({ _id: checkboxId });
+      await foundList.save();
     }
-    
+    res.redirect(listName === formattedDate ? "/" : "/" + listName);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
-  });
-
+  console.log(`Listening on port ${port}`);
+});
 
 function formatDate(date) {
-    const months = [
-        "January", "February", "March", "April",
-        "May", "June", "July", "August",
-        "September", "October", "November", "December"
-    ];
+  const months = [
+    "January", "February", "March", "April",
+    "May", "June", "July", "August",
+    "September", "October", "November", "December"
+  ];
 
-    const days = [
-        "Sunday", "Monday", "Tuesday", "Wednesday",
-        "Thursday", "Friday", "Saturday"
-    ];
+  const days = [
+    "Sunday", "Monday", "Tuesday", "Wednesday",
+    "Thursday", "Friday", "Saturday"
+  ];
 
-    const month = months[date.getMonth()];
-    const dayNumber = date.getDate();
-    const dayName = days[date.getDay()];
+  const month = months[date.getMonth()];
+  const dayNumber = date.getDate();
+  const dayName = days[date.getDay()];
 
-    const formattedString = `${dayName}, ${month} ${dayNumber}`;
-
-    return formattedString;
+  return `${dayName}, ${month} ${dayNumber}`;
 }
 
 const currentDate = new Date();
